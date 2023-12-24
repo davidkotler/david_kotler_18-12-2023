27e5f1dcfddba308d7ce608e8c9c7af4b73b220c
davidkotler <davidkotler9@gmail.com>
2023-12-24 09:55:34 +0200

    api change

--- src/settings.js
+++ src/settings.js
@@ -3,8 +3,7 @@ import CloudIcon from "@mui/icons-material/Cloud";
 import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
 
 export const apiUrl = "https://dataservice.accuweather.com";
-
-export const apiKey = "kUriycj9Nd4CxCKftcouKKGJdyHnulUd";
+export const apiKey = "uGaxZEVwWDzY11HuXgT8LPfCpDDR3JYI";
 
 export const defaultAreaId = 215854;
 
